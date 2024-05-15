f902298381a56f528d1a7063214d2265fecdc9d7
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2024-05-15 23:45:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:49355','http://localhost:52934' ];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:63179','http://localhost:52934' ];
 
 app.use(cors({
   origin: (origin, callback) => {
